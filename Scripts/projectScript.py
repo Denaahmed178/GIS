c164217cfad32788cc150444578d345ded01754d
dina ahmed zakaria <83669926+Denaahmed178@users.noreply.github.com>
2024-04-24 17:38:58 +0200

    Update projectScript.py

--- Scripts/projectScript.py
+++ Scripts/projectScript.py
@@ -8,7 +8,7 @@ print(featureClasses)
 
 # Data initialize
 #place
-palaces = r".\Data\ne_10m_populated_places.shp"
+places = r".\Data\ne_10m_populated_places.shp"
 countries = r".\Data\ne_10m_admin_0_countries.shp"
 geographicLines = r".\Data\ne_10m_geographic_lines.shp"
 disputedAreas = r".\Data\ne_10m_admin_0_disputed_areas.shp"
@@ -18,21 +18,21 @@ Lakes = r".\Data\ne_10m_lakes.shp"
 
 # output file
 output_path = r"C:\Users\green\Desktop\project-gis\Output"
-# cities & disputedAreas in Morocco
 
-arcpy.MakeFeatureLayer_management(palaces, "Palaces_layer")
+# point(2) cities & disputedAreas in Morocco
+arcpy.MakeFeatureLayer_management(places, "Places_layer")
 arcpy.MakeFeatureLayer_management(disputedAreas, "disputedAreas_layer")
 
 arcpy.MakeFeatureLayer_management(countries, "Morocco_layer", """ "NAME" = 'Morocco' """)
 
-arcpy.SelectLayerByLocation_management("Palaces_layer", "WITHIN", "Morocco_layer")
+arcpy.SelectLayerByLocation_management("Places_layer", "WITHIN", "Morocco_layer")
 arcpy.SelectLayerByLocation_management("disputedAreas_layer", "WITHIN", "Morocco_layer")
 
 arcpy.FeatureClassToFeatureClass_conversion("Palaces_layer", output_path, "cities_in_Morocco")
 arcpy.FeatureClassToFeatureClass_conversion("disputedAreas_layer", output_path, "disputedAreas_in_Morocco")
 
 # print the name of cities
-with arcpy.da.SearchCursor("Palaces_layer", "NAME") as cities_in_Morocco_cursor:
+with arcpy.da.SearchCursor("Places_layer", "NAME") as cities_in_Morocco_cursor:
     print ("cities in Morocco")
     for row in cities_in_Morocco_cursor:
         print(row[0])
@@ -42,7 +42,7 @@ with arcpy.da.SearchCursor("Palaces_layer", "NAME") as cities_in_Morocco_cursor:
 #     for row in disputedAreas_in_Morocco_cursor:
 #         print(row[0])
 
-# lakes in africa
+# point(3) lakes in africa
 arcpy.MakeFeatureLayer_management(Lakes, "lakes_layer")
 arcpy.MakeFeatureLayer_management(countries, "Africa_layer", """ "CONTINENT" = 'Africa' """)
 arcpy.SelectLayerByLocation_management("lakes_layer", "INTERSECT", "Africa_layer")
