3da183b3fa0755f46cca0a659ffe5ca230809505
dina ahmed zakaria <83669926+Denaahmed178@users.noreply.github.com>
2024-05-07 17:57:34 +0300

    Update projectScript.py
    Points 7,8,9 has been added to the script

--- Scripts/projectScript.py
+++ Scripts/projectScript.py
@@ -73,7 +73,89 @@ for lake in cities_cursor:
     print(lake.getValue('name'))
     print(lake.getValue('scalerank'))
     print(lake.getValue('wikidataid')) +"\n"
+###################
+# point 7
+# arcpy.MakeFeatureLayer_management(Lakes,'lake')
+lakes_cursor=arcpy.UpdateCursor(Lakes, ['wikidataid','note'])
+for x in lakes_cursor:
+    if x.getValue('wikidataid')=='':
+        x.setValue('wikidataid',"undefined")
+        x.setValue('note', "this row is updated")
+        lakes_cursor.updateRow(x)
+        #print(x[1])
+ arcpy.FeatureClassToFeatureClass_conversion('lake', output_path, 'cities_in{}'.format(x.getValue('note')))
+        #rint x.getValue('note')
 
+# point 8
+countries_fields = ['Germany', 'Egypt', 'Brazil']
+
+# Create a search cursor for the boundary lines
+boundary_cursor = arcpy.SearchCursor(boundary_lines_land, ['ADM0_LEFT', 'ADM0_LEFT'])
+
+# Iterate through the search cursor and create shapefiles for the left and right borders
+for row in boundary_cursor:
+
+    left_country = row.getValue("ADM0_LEFT")  # Get the value of the 'ADMO_LEFT' field
+    right_country = row.getValue("ADM0_RIGHT")  # Get the value of the 'ADMO_RIGHT' field
+    # Check if the left country is in the list of countries
+    if left_country in countries_fields:
+        # Generate a unique feature layer name for the left country
+        left_layer_name = 'Left_' + left_country.replace(" ",
+                                                         "_")  # Replace spaces with underscores for valid layer name
+        # Create a feature layer for the left country
+        arcpy.MakeFeatureLayer_management(boundary_lines_land, left_layer_name,
+                                          """ "ADM0_LEFT" = '{}'""".format(left_country))
+        # Convert the feature layer to a shapefile
+        arcpy.FeatureClassToFeatureClass_conversion(left_layer_name, output_path, 'left_border_{}'.format(left_country))
+
+    # Check if the right country is in the list of countries
+    if right_country in countries_fields:
+        # Generate a unique feature layer name for the right country
+        right_layer_name = 'Right_' + right_country.replace(" ",
+                                                            "_")  # Replace spaces with underscores for valid layer name
+        # Create a feature layer for the right country
+        arcpy.MakeFeatureLayer_management(boundary_lines_land, right_layer_name,
+                                          """ "ADM0_RIGHT" = '{}'""".format(right_country))
+        # Convert the feature layer to a shapefile
+        arcpy.FeatureClassToFeatureClass_conversion(right_layer_name, output_path,
+                                                    'right_border_{}'.format(right_country))
+# point 9
+# tool for point 8
+boundary_lines_land = arcpy.GetParameterAsText(1)
+countries = arcpy.GetParameterAsText(0)
+output_path = arcpy.GetParameterAsText(2)
+country_field = arcpy.GetParameterAsText(3)
+boundary_cursor = arcpy.SearchCursor(boundary_lines_land, ['ADM0_LEFT', 'ADM0_LEFT'])
+
+# Iterate through the search cursor and create shapefiles for the left and right borders
+for row in boundary_cursor:
+
+    left_country = row.getValue("ADM0_LEFT")  # Get the value of the 'ADMO_LEFT' field
+    right_country = row.getValue("ADM0_RIGHT")  # Get the value of the 'ADMO_RIGHT' field
+    # Check if the left country is in the list of countries
+    if left_country in country_field:
+        # Generate a unique feature layer name for the left country
+        left_layer_name = 'Left_' + left_country.replace(" ",
+                                                         "_")  # Replace spaces with underscores for valid layer name
+        # Create a feature layer for the left country
+        arcpy.MakeFeatureLayer_management(boundary_lines_land, left_layer_name,
+                                          """ "ADM0_LEFT" = '{}'""".format(left_country))
+        # Convert the feature layer to a shapefile
+        arcpy.FeatureClassToFeatureClass_conversion(left_layer_name, output_path, 'left_border_{}'.format(left_country))
+
+    # Check if the right country is in the list of countries
+    if right_country in country_field:
+        # Generate a unique feature layer name for the right country
+        right_layer_name = 'Right_' + right_country.replace(" ",
+                                                            "_")  # Replace spaces with underscores for valid layer name
+        # Create a feature layer for the right country
+        arcpy.MakeFeatureLayer_management(boundary_lines_land, right_layer_name,
+                                          """ "ADM0_RIGHT" = '{}'""".format(right_country))
+        # Convert the feature layer to a shapefile
+        arcpy.FeatureClassToFeatureClass_conversion(right_layer_name, output_path,
+                                                    'right_border_{}'.format(right_country))
+
+arcpy.AddMessage("the boundries has been printed ")
 #############################################################################################################################
 
 ### points 10,11
